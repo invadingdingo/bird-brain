cecce5a51f5590bf13cbde0eb2d5ba735ede9fbe
RaymondScottSmith <rayscottsmith1@gmail.com>
2024-11-18 18:41:59 -0600

    Level 5 feature complete

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -41,8 +41,9 @@ public class AudioManager : MonoBehaviour {
         { 
             Destroy(this); 
         } 
-        else 
-        { 
+        else
+        {
+            DontDestroyOnLoad(this);
             instance = this; 
         } 
     }
